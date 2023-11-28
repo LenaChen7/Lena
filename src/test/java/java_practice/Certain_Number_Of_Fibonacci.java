1d2701ac8a0f65284c7d68fe4a4ea52a843e3f81
Lena Chen <lenachen@LenadeAir.fios-router.home>
2023-11-27 21:24:13 -0500

    Fibonacci

--- src/test/java/java_practice/Certain_Number_Of_Fibonacci.java
+++ src/test/java/java_practice/Certain_Number_Of_Fibonacci.java
@@ -7,12 +7,36 @@ public class Certain_Number_Of_Fibonacci {
 
 	public static void main(String[] args) {
 		// print a certain number of fibonacci sequence
-		int howManyNumber = 2;
-		System.out.println(getFibonacci(howManyNumber));
+		int howManyNumber = 10;
+//		System.out.println(getFibonacci1(howManyNumber));
+		System.out.println(Arrays.toString(getFibonacci2(howManyNumber)));
 
 	}
 
-	private static ArrayList<Integer> getFibonacci(int howManyNumber) {
+	private static int[] getFibonacci2(int howManyNumber) {
+
+		int[] Fibonacci = new int[howManyNumber];
+
+		if (howManyNumber == 1) {
+			Fibonacci[0] = 0;
+
+		} else if (howManyNumber == 2) {
+			Fibonacci[0] = 0;
+			Fibonacci[1] = 1;
+		} else {
+			Fibonacci[0] = 0;
+			Fibonacci[1] = 1;
+			for (int i = 2; i < Fibonacci.length; i++) {
+				Fibonacci[i] = Fibonacci[i - 1] + Fibonacci[i - 2];
+			}
+
+		}
+
+		return Fibonacci;
+
+	}
+
+	private static ArrayList<Integer> getFibonacci1(int howManyNumber) {
 		ArrayList<Integer> Fibonacci = new ArrayList<>();
 		if (howManyNumber == 1) {
 			Fibonacci.add(0);
