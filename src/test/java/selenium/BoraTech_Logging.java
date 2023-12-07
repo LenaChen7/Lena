ebcfaf684639b4085ba66a99561e0399d2068160
Lena Chen <lenachen@LenadeAir.fios-router.home>
2023-12-07 12:24:38 -0500

    Add experience

--- src/test/java/selenium/BoraTech_Logging.java
+++ src/test/java/selenium/BoraTech_Logging.java
@@ -1,10 +1,7 @@
 package selenium;
 
-import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BoraTech_Logging {
