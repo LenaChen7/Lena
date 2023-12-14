ec4bc4baca6c7fe1e3e5ea706f84c14215cfd9f0
Lena Chen <lenachen@LenadeAir.fios-router.home>
2023-12-14 11:13:35 -0500

    update Application Form Test base

--- src/test/java/selenium/ApplicationForm.java
+++ src/test/java/selenium/ApplicationForm.java
@@ -1,10 +1,53 @@
 package selenium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import utility.Keywords;
+
 public class ApplicationForm {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		String url = "https://boratech-practice-app.onrender.com/";
+		String firstName = "Lena";
+		String lastName = "Chen";
+		String dob = "07/01/1995";
+		String email = "chenlena@outlook.com";
+		String phone = "";
 
+		try {
+			driver.get(url);
+			driver.findElement(By.linkText("Apply Now")).click();
+			Keywords.wait(2);
+
+			// 1. Validate URL has /apply at the end
+			String actualUrl = driver.getCurrentUrl();
+			if (!actualUrl.endsWith("/apply")) {
+				throw new Exception("Expected to have '/apply' in the url." + "\nCurrent: " + actualUrl);
+			}
+
+			Keywords.wait(2);
+
+			// 2. Validate Title Text
+			String expectedTitleText = "Application Form";
+			String actualTitleText = driver.findElement(By.xpath("//h1[@class='large text-primary']")).getText();
+			if (!actualTitleText.equals(expectedTitleText)) {
+				throw new Exception("Title Text doesn't match.\n" + "Expected: " + expectedTitleText + "\nActual: "
+						+ actualTitleText);
+			}
+
+			System.out.println("Test Passed");
+		} catch (Exception e) {
+			System.out.println("Test Failed");
+			System.out.println(e.getMessage());
+			e.getStackTrace();
+
+		} finally {
+			driver.close();
+			driver.quit();
+		}
 	}
 
 }
--- src/test/java/selenium/Test.java
+++ src/test/java/selenium/Test.java
@@ -3,26 +3,34 @@ package selenium;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import utility.Keywords;
+
 public class Test {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 10; i++) {
-			System.out.println(getTimestamp());  
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 
+		WebDriver driver = new ChromeDriver();
+
+		try {
+			driver.get("https://www.barnesandnoble.com/b/books/_/N-29Z8q8");
+
+			Keywords.wait(2);
+
+			driver.navigate().back();
+			
+			Keywords.wait(2);
+
+		} catch (Exception e) {
+			System.out.println("Test Fail");
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		} finally {
+			driver.close();
+			driver.quit();
 		}
-
-	}
-
-	public static String getTimestamp() {
-		
-		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-		return timestamp.getTime() + "";
 	}
 
 }
