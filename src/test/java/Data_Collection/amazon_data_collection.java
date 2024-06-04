e40fdff9b73362fea7e457fb54e8c3da8d353d59
Lena Chen <lenachen@lenadeair.mynetworksettings.com>
2024-06-04 12:55:42 -0400

    Amazon_Data_Collection

--- src/test/java/Data_Collection/amazon_data_collection.java
+++ src/test/java/Data_Collection/amazon_data_collection.java
@@ -1,6 +1,7 @@
 package Data_Collection;
-
 import java.time.Duration;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -22,18 +23,51 @@ public class amazon_data_collection {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 		driver.manage().window().maximize();
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofMinutes(1));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 
 		String searchTerm = "shampoo";
 
 		try {
 			driver.get("https://www.amazon.com/");
 			driver.findElement(By.cssSelector("#twotabsearchtextbox")).sendKeys(searchTerm + Keys.ENTER);
+			// to check if the element exist
 			Keywords.doesElementExist(driver, By.xpath(
 					"//span[@data-component-type='s-result-info-bar']" + "//*[contains(text(),'" + searchTerm + "')]"),
 					"unable to locate the result page");
 			Keywords.wait(3);
-//			wait.until(ExpectedConditions.visibilityOf(null))
+
+			int counter = 0;
+			while (counter < 200) {
+				String parentXpath = "(//div[@data-component-type='s-search-result'])";
+				List<WebElement> cards = wait
+						.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath(parentXpath), 48));
+
+				for (int i = 1; i <= cards.size(); i++) {
+					String titleXpath = parentXpath + "[" + i + "]//h2";
+					String priceXpath = parentXpath + "[" + i + "]//span[@class = 'a-price']";
+					String price = null;
+
+					String title = driver.findElement(By.xpath(titleXpath)).getText();
+					try {
+						price = driver.findElement(By.xpath(priceXpath)).getText();
+						price = price.replace("\n", ".");
+					} catch (NoSuchElementException e) {
+						continue;
+					}
+
+					System.out.println("ID: " + ++counter + " Price: " + price + " Title: " + title);
+					if (counter == 200) {
+						break;
+					}
+
+				}
+				if (counter == 200) {
+					break;
+				}
+
+				driver.findElement(By.xpath("//a[contains(@class, 's-pagination-next')]")).click();
+
+			}
 
 			System.out.println("Test Passed");
 		} catch (Exception e) {
